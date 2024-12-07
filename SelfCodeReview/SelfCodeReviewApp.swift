57095db5d8b6702853ce34a649f55f97da669302
gjlee <gjlee@rsupport.com>
2024-12-07 17:06:46 +0900

    Initial Commit

---
+++ SelfCodeReview/SelfCodeReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfCodeReviewApp.swift
+//  SelfCodeReview
+//
+//  Created by 이건준 on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SelfCodeReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
